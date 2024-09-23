986372f42057aef04a873a8ed441ffc2ab562b2c
PLUM IS HAPPY <32700040+poeyjeong@users.noreply.github.com>
2024-09-24 01:35:53 +0700

    Update history.dart

--- views/history.dart
+++ views/history.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:group_assign/models/post_model.dart';
+import 'package:group_assign/widgets/post_list.dart';
 
-class Profile extends StatelessWidget {
-  const Profile({super.key});
+class HistoryPage extends StatelessWidget {
+  final List<Post> posts; // Your posts from data.json
+
+  const HistoryPage({super.key, required this.posts});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Profile'),
+        title: const Text("Your Posts"),
       ),
-      body: const Center(
-        child: Text('Profile Page'),
-      ),
+      body: PostList(posts: posts, showOnlyPlumPosts: true), // Show only Plum's posts
     );
   }
 }
