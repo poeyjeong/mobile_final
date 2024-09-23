1d6ad2dbab7be894976494a9c8f27f13d2f6246b
PLUM IS HAPPY <32700040+poeyjeong@users.noreply.github.com>
2024-09-24 01:34:53 +0700

    Update posts.dart
    แก้ไขการแสดงผล non-English language

--- models/posts.dart
+++ models/posts.dart
@@ -1,85 +1,67 @@
-// To parse this JSON data, do
-//
-//     final postdetails = postdetailsFromJson(jsonString);
-
 import 'dart:convert';
 
-Postdetails postdetailsFromJson(String str) => Postdetails.fromJson(json.decode(str));
-
-String postdetailsToJson(Postdetails data) => json.encode(data.toJson());
-
-class Postdetails {
-    List<Post>? posts;
-
-    Postdetails({
-        this.posts,
-    });
-
-    factory Postdetails.fromJson(Map<String, dynamic> json) => Postdetails(
-        posts: json["posts"] == null ? [] : List<Post>.from(json["posts"]!.map((x) => Post.fromJson(x))),
-    );
-
-    Map<String, dynamic> toJson() => {
-        "posts": posts == null ? [] : List<dynamic>.from(posts!.map((x) => x.toJson())),
-    };
+// To parse JSON data into a list of Post objects
+List<Post> postsFromJson(String str) {
+  final jsonData = utf8.decode(str.codeUnits);
+  final jsonMap = jsonDecode(jsonData);
+  return List<Post>.from(jsonMap.map((x) => Post.fromJson(x)));
 }
 
+// To serialize a list of Post objects into JSON format
+String postsToJson(List<Post> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
 class Post {
-    String? id;
-    String? author;
-    String? title;
-    int? likes;
-    int? dislikes;
-    List<Comment>? comments;
-    List<Post>? posts;
+  String id;
+  String author;
+  String title;
+  int likes;
+  int dislikes;
+  List<Comment> comments;
 
-    Post({
-        this.id,
-        this.author,
-        this.title,
-        this.likes,
-        this.dislikes,
-        this.comments,
-        this.posts,
-    });
+  Post({
+    required this.id,
+    required this.author,
+    required this.title,
+    required this.likes,
+    required this.dislikes,
+    required this.comments,
+  });
 
-    factory Post.fromJson(Map<String, dynamic> json) => Post(
-        id: json["id"],
-        author: json["author"],
-        title: json["title"],
-        likes: json["likes"],
-        dislikes: json["dislikes"],
-        comments: json["comments"] == null ? [] : List<Comment>.from(json["comments"]!.map((x) => Comment.fromJson(x))),
-        posts: json["posts"] == null ? [] : List<Post>.from(json["posts"]!.map((x) => Post.fromJson(x))),
-    );
+  factory Post.fromJson(Map<String, dynamic> json) => Post(
+    id: json["id"],
+    author: json["author"],
+    title: json["title"],
+    likes: json["likes"],
+    dislikes: json["dislikes"],
+    comments: List<Comment>.from(json["comments"].map((x) => Comment.fromJson(x))),
+  );
 
-    Map<String, dynamic> toJson() => {
-        "id": id,
-        "author": author,
-        "title": title,
-        "likes": likes,
-        "dislikes": dislikes,
-        "comments": comments == null ? [] : List<dynamic>.from(comments!.map((x) => x.toJson())),
-        "posts": posts == null ? [] : List<dynamic>.from(posts!.map((x) => x.toJson())),
-    };
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "author": author,
+    "title": title,
+    "likes": likes,
+    "dislikes": dislikes,
+    "comments": List<dynamic>.from(comments.map((x) => x.toJson())),
+  };
 }
 
 class Comment {
-    String? author;
-    String? content;
+  String author;
+  String content;
 
-    Comment({
-        this.author,
-        this.content,
-    });
+  Comment({
+    required this.author,
+    required this.content,
+  });
 
-    factory Comment.fromJson(Map<String, dynamic> json) => Comment(
-        author: json["author"],
-        content: json["content"],
-    );
+  factory Comment.fromJson(Map<String, dynamic> json) => Comment(
+    author: json["author"],
+    content: json["content"],
+  );
 
-    Map<String, dynamic> toJson() => {
-        "author": author,
-        "content": content,
-    };
+  Map<String, dynamic> toJson() => {
+    "author": author,
+    "content": content,
+  };
 }
