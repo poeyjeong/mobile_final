c376f4b655bf43b9813f0f43e03e1f21374e051e
PLUM IS HAPPY <32700040+poeyjeong@users.noreply.github.com>
2024-09-26 22:42:21 +0700

    Update post_list.dart

--- widgets/post_list.dart
+++ widgets/post_list.dart
@@ -1,28 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:group_assign/models/post_model.dart';
+import 'package:mobile_final/models/post_model.dart';
+import 'package:mobile_final/widgets/post_stat.dart';
 
 class PostList extends StatelessWidget {
   final List<Post> posts;
+  final bool showOnlyPlumPosts;
 
-  const PostList({super.key, required this.posts, required bool showOnlyPlumPosts});
+  const PostList({super.key, required this.posts, required this.showOnlyPlumPosts});
 
   @override
   Widget build(BuildContext context) {
+    final filteredPosts = showOnlyPlumPosts
+        ? posts.where((post) => post.author == 'Plum').toList()
+        : posts;
+
     return ListView.separated(
-      itemCount: posts.length,
+      itemCount: filteredPosts.length,
       separatorBuilder: (context, index) => const Divider(),
       itemBuilder: (context, index) {
-        final post = posts[index];
+        final post = filteredPosts[index];
         return ListTile(
           title: Text(post.title),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Likes: ${post.likes}'),
-              Text('Dislikes: ${post.dislikes}'),
-              Text('Comments: ${post.comments.length}'),
-            ],
-          ),
+          subtitle: PostStats(post: post),
           onTap: () {
             Navigator.pushNamed(
               context,
