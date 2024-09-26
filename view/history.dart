705dabe9551008e4b0186f01d9ef260a4649b3d1
PLUM IS HAPPY <32700040+poeyjeong@users.noreply.github.com>
2024-09-27 02:35:29 +0700

    Update history.dart

--- view/history.dart
+++ view/history.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:mobile_final/models/post_model.dart';
 import 'package:mobile_final/view/app_bar.dart';
-import 'package:mobile_final/widgets/post_list.dart';
 import 'package:mobile_final/models/config.dart';
+import 'package:mobile_final/widgets/post_stat.dart';
 
 class HistoryPage extends StatefulWidget {
-  const HistoryPage({super.key, required List<Post> posts});
+  const HistoryPage({super.key});
 
   @override
   _HistoryPageState createState() => _HistoryPageState();
@@ -17,20 +17,15 @@ class _HistoryPageState extends State<HistoryPage> {
   List<Post> _posts = [];
   bool _isLoading = true;
 
-  @override
-  void initState() {
-    super.initState();
-    _fetchPosts();
-  }
-
   Future<void> _fetchPosts() async {
     final response =
         await http.get(Uri.parse('http://${Configure.server}/posts'));
 
     if (response.statusCode == 200) {
       final List<Post> posts = postsFromJson(response.body);
+      final plumPosts = posts.where((post) => post.author == 'Plum').toList();
       setState(() {
-        _posts = posts.where((post) => post.author == 'plum').toList();
+        _posts = plumPosts;
         _isLoading = false;
       });
     } else {
@@ -41,15 +36,56 @@ class _HistoryPageState extends State<HistoryPage> {
     }
   }
 
+  void _goBackToHome() {
+    Navigator.pushNamed(context, '/').then((_) {
+      _fetchPosts();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchPosts();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _isLoading
           ? const Center(child: CircularProgressIndicator())
-          : PostList(posts: _posts, showOnlyPlumPosts: true),
-      bottomNavigationBar: MyBottomNavigationBar(
+          : PostList(posts: _posts),
+      bottomNavigationBar: const MyBottomNavigationBar(
         currentIndex: 1,
       ),
+    );
+  }
+}
+
+// post_list.dart
+class PostList extends StatelessWidget {
+  final List<Post> posts;
+
+  const PostList({super.key, required this.posts});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemCount: posts.length,
+      separatorBuilder: (context, index) => const Divider(),
+      itemBuilder: (context, index) {
+        final post = posts[index];
+        return ListTile(
+          title: Text(post.title),
+          subtitle: PostStats(post: post),
+          onTap: () {
+            Navigator.pushNamed(
+              context,
+              '/postdetails',
+              arguments: {'post': post},
+            );
+          },
+        );
+      },
     );
   }
 }
