c44f8a2051f7682abe747e393d63212b9562de3d
PLUM IS HAPPY <32700040+poeyjeong@users.noreply.github.com>
2024-09-26 22:48:30 +0700

    Update history.dart

--- view/history.dart
+++ view/history.dart
@@ -1,21 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:group_assign/models/post_model.dart';
-import 'package:group_assign/widgets/post_list.dart';
+import 'package:mobile_final/models/post_model.dart';
+import 'package:mobile_final/widgets/post_list.dart';
+import 'package:http/http.dart' as http;
+import 'package:mobile_final/models/config.dart';
 
-class HistoryPage extends StatelessWidget {
-  final List<Post> posts;
+class HistoryPage extends StatefulWidget {
+  const HistoryPage({super.key, required List<Post> posts});
 
-  const HistoryPage({super.key, required this.posts});
+  @override
+  _HistoryPageState createState() => _HistoryPageState();
+}
+
+class _HistoryPageState extends State<HistoryPage> {
+  List<Post> _posts = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchPosts();
+  }
+
+  Future<void> _fetchPosts() async {
+    final response =
+        await http.get(Uri.parse('http://${Configure.server}/posts'));
+
+    if (response.statusCode == 200) {
+      final List<Post> posts = postsFromJson(response.body);
+      setState(() {
+        _posts = posts;
+        _isLoading = false;
+      });
+    } else {
+      setState(() {
+        _isLoading = false;
+      });
+      print('Failed to load posts');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    final plumPosts = posts.where((post) => post.author == 'Plum').toList();
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("Your Posts"),
       ),
-      body: PostList(posts: plumPosts, showOnlyPlumPosts: false),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : PostList(posts: _posts, showOnlyPlumPosts: true),
     );
   }
 }
