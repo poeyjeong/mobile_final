921d8e8bc1c162cad664ee1de70a64b01485c2e9
PLUM IS HAPPY <32700040+poeyjeong@users.noreply.github.com>
2024-09-26 22:31:57 +0700

    Update post_list.dart

--- widgets/post_list.dart
+++ widgets/post_list.dart
@@ -1,45 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:group_assign/models/post_model.dart';
 
 class PostList extends StatelessWidget {
   final List<Post> posts;
-  final bool showOnlyPlumPosts;
 
-  const PostList({
-    super.key,
-    required this.posts,
-    required this.showOnlyPlumPosts,
-  });
+  const PostList({super.key, required this.posts, required bool showOnlyPlumPosts});
 
   @override
   Widget build(BuildContext context) {
-    final filteredPosts = showOnlyPlumPosts
-        ? posts.where((post) => post.author == 'Plum').toList()
-        : posts;
-
     return ListView.separated(
-      itemCount: filteredPosts.length,
+      itemCount: posts.length,
       separatorBuilder: (context, index) => const Divider(),
       itemBuilder: (context, index) {
-        final post = filteredPosts[index];
+        final post = posts[index];
         return ListTile(
           title: Text(post.title),
-          subtitle: Row(
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Icon(Icons.thumb_up, size: 16),
-              Text(' ${post.likes} | '),
-              const Icon(Icons.thumb_down, size: 16),
-              Text(' ${post.dislikes} | '),
-              const Icon(Icons.comment, size: 16),
-              Text(' ${post.comments.length}'),
+              Text('Likes: ${post.likes}'),
+              Text('Dislikes: ${post.dislikes}'),
+              Text('Comments: ${post.comments.length}'),
             ],
           ),
           onTap: () {
             Navigator.pushNamed(
               context,
-              '/post_detail',
-              arguments: post,
+              '/postdetails',
+              arguments: {'post': post},
             );
           },
         );
