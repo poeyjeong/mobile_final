88aa22ada51f96977351c5e8792364a4a062ce06
PLUM IS HAPPY <32700040+poeyjeong@users.noreply.github.com>
2024-09-27 02:35:47 +0700

    Update post.dart

--- view/post.dart
+++ view/post.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:group_assign/widgets/post_editor.dart';
+import 'package:mobile_final/widgets/post_editor.dart';
 
 class NewPostPage extends StatelessWidget {
   const NewPostPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const PostEditorPage();
+    return const PostEditorPage(post: null,);
   }
 }
