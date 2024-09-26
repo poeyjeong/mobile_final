8b53965f2c0c5441b33aea60cbd03b1f13db3ab9
PLUM IS HAPPY <32700040+poeyjeong@users.noreply.github.com>
2024-09-26 17:14:30 +0700

    Update post.dart

--- view/post.dart
+++ view/post.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:group_assign/widgets/post_editor.dart'; // Make sure this import is correct
+import 'package:group_assign/widgets/post_editor.dart';
 
 class NewPostPage extends StatelessWidget {
   const NewPostPage({super.key});
