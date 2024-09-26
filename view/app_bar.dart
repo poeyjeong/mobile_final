f7e884f12fa22b582b66c8fe8de585e0a587ccfb
PLUM IS HAPPY <32700040+poeyjeong@users.noreply.github.com>
2024-09-26 17:19:29 +0700

    Update app_bar.dart
    เพิ่ม MyBottomNavigationBar

--- view/app_bar.dart
+++ view/app_bar.dart
@@ -1,15 +1,44 @@
 // app_bar.dart
 import 'package:flutter/material.dart';
+import 'package:group_assign/view/history.dart';
 
-class MyAppBar extends StatelessWidget {
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const MyAppBar({super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(56);
+
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      backgroundColor: Color.fromARGB(255, 102, 19, 211),
+      backgroundColor: const Color.fromARGB(255, 102, 19, 211),
       title: const Text(
         "Hello Plum",
         style: TextStyle(fontSize: 24, color: Colors.white),
       ),
+    );
+  }
+}
+class MyBottomNavigationBar extends StatelessWidget {
+  const MyBottomNavigationBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.history), label: "History"),
+      ],
+      onTap: (index) {
+        if (index == 1) {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const HistoryPage(posts: []), // Pass an empty list for now
+              )
+          ); // Navigate to history page
+        }
+      },
     );
   }
 }
