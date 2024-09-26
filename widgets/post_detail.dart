c644077eb0d6c3c3acfbbbf10230cfb35f09cd62
PLUM IS HAPPY <32700040+poeyjeong@users.noreply.github.com>
2024-09-26 22:30:53 +0700

    Update post_detail.dart

--- widgets/post_detail.dart
+++ widgets/post_detail.dart
@@ -1,51 +1,37 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:group_assign/models/config.dart';
-import 'package:http/http.dart' as http;
 import 'package:group_assign/models/post_model.dart';
 
-class PostEditorPage extends StatefulWidget {
-  const PostEditorPage({super.key});
+class PostList extends StatelessWidget {
+  final List<Post> posts;
 
-  @override
-  _PostEditorPageState createState() => _PostEditorPageState();
-}
-
-class _PostEditorPageState extends State<PostEditorPage> {
-  List<Post> _posts = []; // Assume you have some posts to edit
-  // Add your editing logic here
-
-  Future<void> _savePosts() async {
-    String jsonData = postsToJson(_posts);
-
-    final response = await http.post(
-      Uri.parse('http://${Configure.server}/update_posts'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: utf8.encode(jsonData),
-    );
-
-    if (response.statusCode == 200) {
-      // Handle successful update
-    } else {
-      // Handle error
-    }
-  }
+  const PostList({super.key, required this.posts, required bool showOnlyPlumPosts});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Post Editor'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: _savePosts,
-          child: const Text('Save Posts'),
-        ),
-      ),
+    return ListView.separated(
+      itemCount: posts.length,
+      separatorBuilder: (context, index) => const Divider(),
+      itemBuilder: (context, index) {
+        final post = posts[index];
+        return ListTile(
+          title: Text(post.title),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Likes: ${post.likes}'),
+              Text('Dislikes: ${post.dislikes}'),
+              Text('Comments: ${post.comments.length}'),
+            ],
+          ),
+          onTap: () {
+            Navigator.pushNamed(
+              context,
+              '/postdetails',
+              arguments: {'post': post},
+            );
+          },
+        );
+      },
     );
   }
 }
