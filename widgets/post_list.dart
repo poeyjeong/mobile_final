5fd22f68f5b17a6d6b99ac9b8c13802e34bd720c
PLUM IS HAPPY <32700040+poeyjeong@users.noreply.github.com>
2024-09-27 02:36:44 +0700

    Update post_list.dart

--- widgets/post_list.dart
+++ widgets/post_list.dart
@@ -4,21 +4,16 @@ import 'package:mobile_final/widgets/post_stat.dart';
 
 class PostList extends StatelessWidget {
   final List<Post> posts;
-  final bool showOnlyPlumPosts;
 
-  const PostList({super.key, required this.posts, required this.showOnlyPlumPosts});
+  const PostList({super.key, required this.posts});
 
   @override
   Widget build(BuildContext context) {
-    final filteredPosts = showOnlyPlumPosts
-        ? posts.where((post) => post.author == 'Plum').toList()
-        : posts;
-
     return ListView.separated(
-      itemCount: filteredPosts.length,
+      itemCount: posts.length,
       separatorBuilder: (context, index) => const Divider(),
       itemBuilder: (context, index) {
-        final post = filteredPosts[index];
+        final post = posts[index];
         return ListTile(
           title: Text(post.title),
           subtitle: PostStats(post: post),
