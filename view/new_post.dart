188769c1ddb95c97a3dfa10b6ef45299e926009f
PLUM IS HAPPY <32700040+poeyjeong@users.noreply.github.com>
2024-09-28 20:31:11 +0700

    Update and rename post.dart to new_post.dart

--- view/new_post.dart
+++ view/new_post.dart
@@ -1,3 +1,4 @@
+// new_post.dart
 import 'package:flutter/material.dart';
 import 'package:mobile_final/models/post_model.dart';
 import 'package:mobile_final/widgets/post_edit.dart';
@@ -7,6 +8,11 @@ class NewPostPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PostEditPage(post: Post.fromJson({}));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Create New Post"),
+      ),
+      body: PostEditPage(post: Post.fromJson({})),
+    );
   }
 }
