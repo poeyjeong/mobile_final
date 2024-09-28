735c5e04e17945d981b87ca44c774887a1a54894
PLUM IS HAPPY <32700040+poeyjeong@users.noreply.github.com>
2024-09-28 18:27:27 +0700

    Update post_list.dart

--- widgets/post_list.dart
+++ widgets/post_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http; // Importing the http package
+import 'package:http/http.dart' as http;
 import 'package:mobile_final/models/config.dart';
 import 'package:mobile_final/models/post_model.dart';
 import 'package:mobile_final/widgets/post_stat.dart';
