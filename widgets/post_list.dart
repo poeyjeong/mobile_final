0d45d14be853e00e768a1c277655447d80893009
PLUM IS HAPPY <32700040+poeyjeong@users.noreply.github.com>
2024-09-28 17:34:49 +0700

    Update post_list.dart

--- widgets/post_list.dart
+++ widgets/post_list.dart
@@ -1,11 +1,17 @@
+// post_list.dart
 import 'package:flutter/material.dart';
 import 'package:mobile_final/models/post_model.dart';
 import 'package:mobile_final/widgets/post_stat.dart';
 
 class PostList extends StatelessWidget {
   final List<Post> posts;
+  final bool isHistoryPage; // เอาไว้บอกว่า อยู่ในหน้า History ไหม
 
-  const PostList({super.key, required this.posts});
+  const PostList({
+    super.key,
+    required this.posts,
+    required this.isHistoryPage,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +23,14 @@ class PostList extends StatelessWidget {
         return ListTile(
           title: Text(post.title),
           subtitle: PostStats(post: post),
+          trailing: isHistoryPage
+            ? IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    // ฟังก์ชันแก้ไขเฉพาะใน HistoryPage
+                  },
+                )
+              : null, // ถ้าไม่ใช่ HistoryPage จะไม่มีปุ่มแก้ไข
           onTap: () {
             Navigator.pushNamed(
               context,
